29bd7c95f6850059c5182d25f8efa14ad1a00972
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2019-03-13 16:01:43 -0500

    v1.0.2

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-export function findTestIDInWrapper({ wrapper, testID, findAll = false }) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.findTestIDInWrapper = findTestIDInWrapper;
+exports.flushPromises = flushPromises;
+
+function findTestIDInWrapper(_ref) {
+  var wrapper = _ref.wrapper,
+    testID = _ref.testID,
+    _ref$findAll = _ref.findAll,
+    findAll = _ref$findAll === void 0 ? false : _ref$findAll;
+
   if (findAll) {
-    return wrapper.findAll(`[test-id="${testID}"]`);
+    return wrapper.findAll('[test-id="'.concat(testID, '"]'));
   }
-  return wrapper.find(`[test-id="${testID}"]`);
+
+  return wrapper.find('[test-id="'.concat(testID, '"]'));
 }
 
-export function flushPromises() {
-  return new Promise(resolve => {
+function flushPromises() {
+  return new Promise(function(resolve) {
     return setImmediate(resolve);
   });
 }
